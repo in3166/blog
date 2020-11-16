a83be182018424ca7a188c7dcd4b667ee294dc59
in3166 <in3166@naver.com>
2020-11-16 11:17:31 +0900

    Modify weather function to callback in index

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 //var index = require("../views/index.ejs");
 const app = require("../app.js");
 var covert = require("./converXY");
+var wea = require("./weather");
 const request = require('request');
 const moment = require('moment');
 const cheerio = require('cheerio');
@@ -11,132 +12,8 @@ const cheerio = require('cheerio');
 router.post("/con", function (req, res) {
   let lon = req.body.lon;
   let lat = req.body.lat;
-  let location = covert.toXY(lat, lon);
-  console.log(location.x, location.y);
 
-  const nx = location.x;
-  const ny = location.y;
-  const dataType = 'XML';
-
-  var today = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth() + 1;
-  var yyyy = today.getFullYear();
-  var hours = today.getHours();
-  var minutes = today.getMinutes();
-  console.log("time " + minutes)
-  if (minutes < 30) {
-    // 30분보다 작으면 한시간 전 값
-    hours = hours - 1;
-    if (hours < 0) {
-      // 자정 이전은 전날로 계산
-      today.setDate(today.getDate() - 1);
-      dd = today.getDate();
-      mm = today.getMonth() + 1;
-      yyyy = today.getFullYear();
-      hours = 23;
-    }
-  }
-  if (hours < 10) {
-    hours = '0' + hours
-  }
-  if (mm < 10) {
-    mm = '0' + mm
-  }
-  if (dd < 10) {
-    dd = '0' + dd
-  }
-
-  basedate = yyyy + "" + mm + "" + dd;
-  console.log("12321:  " + hours)
-
-  switch (hours) {
-    case 23:
-    case 24:
-    case 01:
-      hours = 23;
-      break;
-    case 02:
-    case 03:
-    case 04:
-      hours = 02;
-      break;
-    case 05:
-    case 06:
-    case 07:
-      hours = 05;
-      break;
-    case 08:
-    case 09:
-    case 10:
-      hours = 08;
-      break;
-    case 11:
-    case 12:
-    case 13:
-      hours = 11;
-      break;
-    case 14:
-    case 15:
-    case 16:
-      hours = 14;
-      break;
-    case 17:
-    case 18:
-    case 19:
-      hours = 17;
-    case 20:
-    case 21:
-    case 22:
-      hours = 20;
-      break;
-    default:
-      break;
-  }
-
-  console.log("123:  " + hours)
-
-  var _nx = nx,
-    _ny = ny,
-    apikey = "MCY3wIU4Zx8fdOOEsVJdb3iTtG9GeFn1YnYW1I8wmirD%2FEB3nyQZCvcvIeEfLUCIaPJ8pZA0hSfsR8SLUUFVFA%3D%3D",
-    today = yyyy + "" + mm + "" + dd,
-    basetime = hours + "00",
-    fileName = "http://apis.data.go.kr/1360000/VilageFcstInfoService";
-  fileName += "?ServiceKey=" + apikey;
-  fileName += "&base_date=" + today;
-  fileName += "&base_time=" + basetime;
-  fileName += "&nx=" + _nx + "&ny=" + _ny;
-  fileName += "&pageNo=1&numOfRows=6";
-  fileName += "&_type=json";
-
-  console.log(nx, ny)
-  var url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst';
-  var queryParams = '?' + encodeURIComponent('ServiceKey') + '=MCY3wIU4Zx8fdOOEsVJdb3iTtG9GeFn1YnYW1I8wmirD%2FEB3nyQZCvcvIeEfLUCIaPJ8pZA0hSfsR8SLUUFVFA%3D%3D'; /* Service Key*/
-  queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
-  queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('14'); /* */
-  queryParams += '&' + encodeURIComponent('dataType') + '=' + encodeURIComponent('XML'); /* */
-  queryParams += '&' + encodeURIComponent('base_date') + '=' + encodeURIComponent(basedate); /* */
-  queryParams += '&' + encodeURIComponent('base_time') + '=' + encodeURIComponent(hours + '00'); /* */
-  queryParams += '&' + encodeURIComponent('nx') + '=' + encodeURIComponent(nx); /* */
-  queryParams += '&' + encodeURIComponent('ny') + '=' + encodeURIComponent(ny); /* */
-  var data = [];
-
-  request({
-    url: url + queryParams,
-    method: 'GET'
-  }, function (error, response, body) {
-    console.log(body)
-    $ = cheerio.load(body);
-    $('item').each(function (idx) {
-      const time = $(this).find('fcstDate').text();
-      const cate = $(this).find('category').text();
-      const wea_val = $(this).find('fcstValue').text();
-
-      data.push({ "time": time, "cate": cate, "val": wea_val });
-      // 출력
-      console.log(`시간 : ${time} 날씨 정보 : ${cate} 값 : ${wea_val}`);
-    });
-    console.log(data);
+  wea(lat, lon, function (data) {
     res.json(data)
   });
 });
--- routes/weather.js
+++ routes/weather.js
@@ -1,188 +1,151 @@
-var http = require('http');
-var async = require('async');
-var querystring = require('querystring');
-var xml2js = require('xml2js');
-
-var http_get = function (url, data, callback) {
-
-    var query = querystring.stringify(data);
-
-    if (query !== '')
-
-        url = url + '&' + query;
-
-    //console.log(url);
-
-    http.get(url, function (res) {
-
-        var body = '';
-
-        res.setEncoding('utf8');
-
-        res.on('readable', function () {
-
-            var chunk = this.read() || '';
-
-            body += chunk;
-
-        });
-
-        res.on('end', function () {
-
-            callback(body);
-
-        });
-
-        res.on('error', function (e) {
-
-            console.log('error', e.message);
-
-        });
-
-    });
-
-};
-
-// 기상청 API를 사용
-
-// http://cloudless.tistory.com/128
-
-// 한국 도시와 법정동을 사용해 X, Y 좌표 구한다.
-
-// top : 도, mid : 시/군/구, leaf : 읍/면/동
-
-var getKoreanWeather = function (top, mid, leaf, callback) {
-
-    async.waterfall([
-
-        // 도/시 검색
-
-        function (callback) {
-
-            http_get('http://www.kma.go.kr/DFSROOT/POINT/DATA/top.json.txt', {}, function (resData) {
-
-                var topObj = JSON.parse(resData);
-
-                for (var i = 0; i < topObj.length; ++i) {
-
-                    if (topObj[i].value.indexOf(top) >= 0) {
-
-                        callback(null, topObj[i]);
-
-                        return;
-
-                    }
-
-                }
-
-                callback('Can not find top', top);
-
-            });
-
-        },
-
-        // 시/구/군 검색
-
-        function (topObj, callback) {
-
-            http_get('http://www.kma.go.kr/DFSROOT/POINT/DATA/mdl.' + topObj.code + '.json.txt', {}, function (resData) {
-
-                var midObj = JSON.parse(resData);
-
-                if (mid === '') {
-
-                    callback(null, topObj, midObj[0]);
-
-                    return;
-
-                } else {
-
-                    for (var i = 0; i < midObj.length; ++i) {
-
-                        if (midObj[i].value.indexOf(mid) >= 0) {
-
-                            callback(null, topObj, midObj[i]);
-
-                            return;
-
-                        }
-
-                    }
-
-                }
-
-                callback('Can not find mid', topObj, mid);
-
-            });
-
-        },
-
-        // 읍/면/동 검색
-
-        function (topObj, midObj, callback) {
-
-            http_get('http://www.kma.go.kr/DFSROOT/POINT/DATA/leaf.' + midObj.code + '.json.txt', {}, function (resData) {
-
-                var leafObj = JSON.parse(resData);
-
-                if (leaf === '') {
-
-                    callback(null, topObj, midObj, leafObj[0]);
-
-                    return;
-
-                } else {
-
-                    for (var i = 0; i < leafObj.length; ++i) {
-
-                        if (leafObj[i].value.indexOf(leaf) >= 0) {
-
-                            callback(null, topObj, midObj, leafObj[i]);
-
-                            return;
-
-                        }
-
-                    }
-
-                }
-
-                callback('Can not find leaf', topObj, midObj, leaf);
-
-            });
-
-        },
-
-        // 날씨 검색
-
-        function (topObj, midObj, leafObj, callback) {
-
-            http_get('http://www.kma.go.kr/wid/queryDFS.jsp?gridx=' + leafObj.x + '&gridy=' + leafObj.y, {}, function (resData) {
-
-                xml2js.parseString(resData, function (err, obj) {
-
-                    if (err) {
-
-                        callback(err, topObj, midObj, leafObj, null);
-
-                    } else {
-
-                        callback(null, topObj, midObj, leafObj, obj.wid.body[0].data[0]);
-
-                    }
-
-                });
-
-            });
-
+var covert = require("./converXY");
+const request = require('request');
+const moment = require('moment');
+const cheerio = require('cheerio');
+
+function weather(lat, lon, callback) {
+
+    let location = covert.toXY(lat, lon);
+    console.log(location.x, location.y);
+
+    const nx = location.x;
+    const ny = location.y;
+    const dataType = 'XML';
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+
+    console.log("time " + minutes)
+
+    if (minutes < 30) {
+        // 30분보다 작으면 한시간 전 값
+        hours = hours - 1;
+        if (hours < 0) {
+            // 자정 이전은 전날로 계산
+            today.setDate(today.getDate() - 1);
+            dd = today.getDate();
+            mm = today.getMonth() + 1;
+            yyyy = today.getFullYear();
+            hours = 23;
         }
+    }
+    if (hours < 10) {
+        hours = '0' + hours
+    }
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+    if (dd < 10) {
+        dd = '0' + dd
+    }
 
-    ], function (error, topObj, midObj, leafObj, weather) {
+    basedate = yyyy + "" + mm + "" + dd;
+    console.log("12321:  " + hours)
 
-        callback(error, topObj, midObj, leafObj, weather);
+    switch (hours) {
+        case 23:
+        case 24:
+        case 01:
+            hours = 23;
+            break;
+        case 02:
+        case 03:
+        case 04:
+            hours = '0' + 2;
+            break;
+        case 05:
+        case 06:
+        case 07:
+            hours = '0' + 5;
+            break;
+        case 08:
+        case 09:
+        case 10:
+            hours = '0' + 8;
+            break;
+        case 11:
+        case 12:
+        case 13:
+            hours = 11;
+            break;
+        case 14:
+        case 15:
+        case 16:
+            hours = 14;
+            break;
+        case 17:
+        case 18:
+        case 19:
+            hours = 17;
+            break;
+        case 20:
+        case 21:
+        case 22:
+            hours = 20;
+            break;
+        default:
+            break;
+    }
 
+    console.log("123:  " + hours)
+
+    var _nx = nx,
+        _ny = ny,
+        apikey = "MCY3wIU4Zx8fdOOEsVJdb3iTtG9GeFn1YnYW1I8wmirD%2FEB3nyQZCvcvIeEfLUCIaPJ8pZA0hSfsR8SLUUFVFA%3D%3D",
+        today = yyyy + "" + mm + "" + dd,
+        basetime = hours + "00",
+        fileName = "http://apis.data.go.kr/1360000/VilageFcstInfoService";
+    fileName += "?ServiceKey=" + apikey;
+    fileName += "&base_date=" + today;
+    fileName += "&base_time=" + basetime;
+    fileName += "&nx=" + _nx + "&ny=" + _ny;
+    fileName += "&pageNo=1&numOfRows=6";
+    fileName += "&_type=json";
+
+    console.log(nx, ny)
+    var url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst';
+    var queryParams = '?' + encodeURIComponent('ServiceKey') + '=MCY3wIU4Zx8fdOOEsVJdb3iTtG9GeFn1YnYW1I8wmirD%2FEB3nyQZCvcvIeEfLUCIaPJ8pZA0hSfsR8SLUUFVFA%3D%3D'; /* Service Key*/
+    queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
+    queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('14'); /* */
+    queryParams += '&' + encodeURIComponent('dataType') + '=' + encodeURIComponent('XML'); /* */
+    queryParams += '&' + encodeURIComponent('base_date') + '=' + encodeURIComponent(basedate); /* */
+    queryParams += '&' + encodeURIComponent('base_time') + '=' + encodeURIComponent(hours + '00'); /* */
+    queryParams += '&' + encodeURIComponent('nx') + '=' + encodeURIComponent(nx); /* */
+    queryParams += '&' + encodeURIComponent('ny') + '=' + encodeURIComponent(ny); /* */
+
+    var data = [];
+
+    request({
+        url: url + queryParams,
+        method: 'GET'
+    }, function (error, response, body) {
+        //  console.log(body);
+        callback(body);
     });
+}
 
-};
+function weather2(lat, lon, call) {
+    weather(lat, lon, function (body) {
+        // console.log(body);
+        let data = [];
+        $ = cheerio.load(body);
+        $('item').each(function (idx) {
+            const time = $(this).find('fcstDate').text();
+            const cate = $(this).find('category').text();
+            const wea_val = $(this).find('fcstValue').text();
 
-exports.getKoreanWeather = getKoreanWeather;
+            data.push({ "time": time, "cate": cate, "val": wea_val });
+            // 출력
+            // console.log(`시간 : ${time} 날씨 정보 : ${cate} 값 : ${wea_val}`);
+        });
+        console.log(data);
+        call(data);
+        return data;
+    })
+}
+
+module.exports = weather2;
